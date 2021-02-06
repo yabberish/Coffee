bd9b8534c92ba66320a1e3091d9cb70358752b51
elf <shadenitey@Gmail.com>
2021-02-06 08:23:57 +0000

    work on infracts command

--- cogs/warn.py
+++ cogs/warn.py
@@ -70,7 +70,7 @@ class WarnCommand(commands.Cog):
                     color = 0x2F3136
 
                 )
-                embed.set_author(name=f"✅ Successfully logged a warn & messaged {member.name} for {reason}.", icon_url=str(member.avatar_url))
+                embed.set_author(name=f"✅ Successfully logged a warn with {member.name} for {reason}.", icon_url=str(member.avatar_url))
                 await ctx.send(embed=embed)
                 author = ctx.author
                 guild = ctx.guild
@@ -84,9 +84,32 @@ class WarnCommand(commands.Cog):
                  send_messages=False, read_messages=False)
                  await asyncio.sleep(2)
                  await msg.edit(content="Done! #coffee-logs")
+                 await ctx.send("If the channel is not created, please create one manually and add permissions to it.")
                 embed = discord.Embed(title="Warn 📝", description=f"**Infractor:** {ctx.author.mention}\n**Reason:** {reason}\n**User warned:**{member.mention}", color=0x2F3136)
 
                 await channel.send(embed=embed)
+
+   """ @commands.command(aliases=['warns', 'infracts'])
+    @commands.guild_only()
+    @commands.has_permissions(manage_messages=True)
+    async def infractions(self, ctx, member: discord.Member):
+        infracts = self.db.fetchrow("SELECT * FROM Warns WHERE user_id=?, reason=?", (user_id, reason))
+        if infracts:
+         return infracts["user_id"]
+        if ctx.author.id in infracts:
+          embed = discord.Embed(
+            color=0x2F3136,
+            description=f"**<@{user_id}>'s Warnings.\n\n\n{reason}**")
+          embed.set_author(name=f"{member.name}'s infractions:", icon_url=str(member.avatar_url))
+          await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                color=0x2F3136,
+            )
+
+            embed.set_author(name=f"{member.name} has no {ctx.invoked_with}!", icon_url=str(member.avatar_url))
+            await ctx.send(embed=embed) """
+        # TODO: Finish infracts command
 
 
 
