d9880d63cce9eadbb6522cb2465446307bcf8a65
elf <shadenitey@Gmail.com>
2021-02-06 07:26:28 +0000

    finish new sqlite system

---
+++ utils/db/create_tables.py
@@ -0,0 +1,24 @@
+from utils.database import sqlite as db
+
+
+class Accounts(db.Table):
+    user_id = db.Column("BIGINT", nullable=False, primary_key=True)
+    bakery_name = db.Column("TEXT", nullable=False)
+    cakes = db.Column("BIGINT", nullable=False, primary_key=True)
+
+
+def creation(debug: bool = False):
+    failed = False
+
+    for table in db.Table.all_tables():
+        try:
+            table.create()
+        except Exception as e:
+            print(f'Could not create {table.__tablename__}.\n\nError: {e}')
+            failed = True
+        else:
+            if debug:
+                print(f'[{table.__module__}] Created {table.__tablename__}.')
+
+
+    return True if not failed else False
