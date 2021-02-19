e14dd0ac8a878f6f68b344381ed14cc91854ff36
elf <49889930+elfq@users.noreply.github.com>
2021-02-19 17:31:40 -0500

    Update logs.py

--- cogs/logs.py
+++ cogs/logs.py
@@ -1,37 +1,47 @@
 import discord
 from discord.ext import commands
-from utils.database import create_tables, sqlite
-from utils import default, checks
-tables = create_tables.creation(debug=True)
+from utils.database import sqlite, create_tables
 
 
-class Setup(commands.Cog):
+class Events(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
     self.db = sqlite.Database()
 
-  @commands.group(invoke_without_command=True)
-  async def setup(self, ctx : commands.Context):
-    await ctx.send("**What would you like to setup for the server?**\n\n**`;;setup logs`** - Setup logging for the server.")  
+  def logs(self, guild_id):
+        data = self.db.fetchrow("SELECT * FROM Logging WHERE guild_id=?", (guild_id,))
+        if data:
+            return data["logs_id"]
+        else:
+            return None
+  
+  @commands.Cog.listener()
+  async def on_message_delete(self, message):
+   log_channel = self.bot.get_channel(self.logs(message.guild.id))
+   if log_channel: 
+     embed = discord.Embed(
+     title="Message Deleted 📝",
+     description=f"**Deleted in:** `#{message.channel}`\n**Author:** `{message.author}`\n**Message:** ```{message.content}```",
+     color=0x2F3136
+     )
+     embed.timestamp = message.created_at
+     await log_channel.send(embed=embed)
+  
+  @commands.Cog.listener()
+  async def on_message_edit(self, before, after):
+   log_channel = self.bot.get_channel(self.logs(before.guild.id))
+   if before.author.bot is True:
+     return None
+   if log_channel:
+     embed = discord.Embed(
+     title="Message Edited 📝",
+     description=f"**Edited in:** `#{before.channel}`\n**Author:** `{before.author}`\n**Before:** ```{before.content}```\n**Now:** ```{after.content}```",
+     color=0x2F3136
+     )
+     embed.timestamp = before.created_at
+     await log_channel.send(embed=embed)
 
-  @setup.command(
-    name = "log",
-    help = "Setup the bot for the server.",
-    aliases=['logs', 'logging'])
-  @commands.has_permissions(manage_guild=True)
-  async def logs(self, ctx : commands.Context):
-    overwrites = {
-        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False, send_messages=False),
-        ctx.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True)
-    }
-    logs = await ctx.guild.create_text_channel('coffee-logs', overwrites=overwrites)
-    log_channel_id = logs.id
-    self.db.execute("INSERT INTO Logging VALUES (?, ?)", (ctx.guild.id, logs.id))
-    await ctx.send(f"Successfully setup the server, <#{logs.id}>")
   
 
-
-
-
 def setup(bot):
- bot.add_cog(Setup(bot))
+  bot.add_cog(Events(bot))
